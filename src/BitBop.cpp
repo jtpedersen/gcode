da834b474ae7f7ba0d9af07cb5699dc4a3daf519
pedersen <jtp@cs.au.dk>
2017-08-25 08:10:50 +0200

    started a ToolKernel thingy

--- src/BitBop.cpp
+++ src/BitBop.cpp
@@ -9,6 +9,34 @@
 #include <QToolBar>
 
 #include <iostream>
+#include <limits>
+
+ToolKernel::ToolKernel(int size) : size(size) { heights.reserve(size * size); };
+
+ToolKernel ToolKernel::endmill(int size) {
+  ToolKernel tk(size);
+  const auto rSq = (size / 2) * (size / 2);
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
+      const auto dx = std::abs(i - size / 2);
+      const auto dy = std::abs(j - size / 2);
+      const auto dSq = dx * dx + dy * dy;
+      const auto idx = i + size * j;
+      tk.heights[idx] = (dSq < rSq) ? 0 : std::numeric_limits<int>::max();
+    }
+  }
+  return tk;
+}
+
+float ToolKernel::get(int i, int j) {
+  const auto x = i + size / 2;
+  const auto y = j + size / 2;
+  if (x < 0 || x >= size || y < 0 || y >= size)
+    return 0.0f;
+  return heights[x + size * y];
+}
+
+int ToolKernel::w() const { return size; }
 
 BitBop::BitBop() {
   setWindowTitle("Bit bop");
@@ -44,42 +72,38 @@ void BitBop::loadFile(const QString &path) {
 }
 
 void BitBop::endMillIt() {
+  auto dst = image.copy();
+  toolConvolve(image, dst, ToolKernel::endmill(4));
+  const auto pixels = QPixmap::fromImage(dst);
+  main->setPixmap(pixels);
+}
 
-  auto eval = [img = image.copy()](int x, int y) {
-    const auto p = img.constBits();
-    const auto stride = img.width();
-    constexpr int N = 4;
-    int sum = 0;
+void BitBop::toolConvolve(const QImage &src, QImage &dst, const ToolKernel tk) {
+
+  auto eval = [&src, &tk, N = tk.w() / 2 ](int x, int y) {
+    const auto p = src.constBits();
+    const auto stride = src.width();
+    int deep = std::numeric_limits<int>::max();
     const auto val = p[x + y * stride];
     for (int j = -N; j <= N; j++) {
       for (int i = -N; i <= N; i++) {
-        const auto dist = (i * i) + (j * j);
-        if (dist < N * N)
-          continue;
         const auto px = x + i;
         const auto py = y + j;
-        if (px < 0 || py < 0 || px >= img.width() || py >= img.height())
-          continue;
+        const auto k = tk.get(i,j);
         const auto cur = p[px + py * stride];
-        if (cur > val)
-          return 255;
-        sum += val - cur;
+        deep = std::max(c
       }
     }
-    return sum / (4*N*N);
+    return deep;
   };
 
   const auto stride = image.width();
   auto bits = image.bits();
   for (int j = 0; j < image.height(); j++) {
     for (int i = 0; i < image.width(); i++) {
-      bits[i + j * stride] = eval(i,j);
+      bits[i + j * stride] = eval(i, j);
     }
   }
-
-  const auto pixels = QPixmap::fromImage(image);
-  main->setPixmap(pixels);
-
 }
 
 int main(int argc, char *argv[]) {
--- src/BitBop.h
+++ src/BitBop.h
@@ -1,11 +1,27 @@
 #ifndef MAINWINDOW_H_
 #define MAINWINDOW_H_
 
-#include <QMainWindow>
 #include <QImage>
+#include <QMainWindow>
+#include <vector>
 
 class QLabel;
 class QToolBar;
+
+class ToolKernel {
+  std::vector<float> heights;
+  int size;
+  ToolKernel(int size);
+public:
+  // Create a flat endmill at size 
+  static ToolKernel endmill(int size);
+  // static ToolKernel vbit();
+  // static ToolKernel ballnose();
+  /// Return depth at i,j z if outsize, centered on size/2 size/2
+  float get(int i, int j);
+  /// width/height its square
+  int w() const;
+};
 
 class BitBop : public QMainWindow {
 public:
@@ -19,6 +35,8 @@ private:
   void loadFile(const QString &fileName);
 
   void endMillIt();
+
+  void toolConvolve(const QImage &src, QImage &dst, const ToolKernel k);
 };
 
 #endif /* !MAINWINDOW_H_ */
